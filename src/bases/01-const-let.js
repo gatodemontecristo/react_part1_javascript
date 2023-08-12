58df470d082330d30871a4cacc1b678ac1e60e72
Erick Dejo Vicente <61488294+mapachefurioso@users.noreply.github.com>
2023-08-11 23:07:44 -0500

    template string y const-let

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,21 @@
+
+
+//Variables y Constantes
+
+const nombre = 'Fernando';
+const apellido = 'Herrera';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre,apellido,valorDado);
+
+if(true){
+    let valorDado = 6;
+    console.log(valorDado);
+
+    const nombre = 'Peter';
+    console.log(nombre);
+}
+
+console.log(valorDado);
